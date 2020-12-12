3de566d932b6c97013ea9db87ce3e75348297653
tanjoin <tanjoin@users.noreply.github.com>
2020-12-13 01:06:08 +0900

    更新が多すぎる気がするので減らす

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    console.log(`${mutation.type} - ${mutation.target.outerHTML} - ${mutation.oldValue}`);
+  });
   try {
     [...document.querySelectorAll('div[aria-label="タイムライン: トレンド"] > div > div')]
       .filter((e) => e.innerText.includes('BuzzFeed') || e.innerText.includes('ハフポスト'))
@@ -10,5 +13,5 @@ const observer = new MutationObserver((mutations) => {
     console.error(e);
   }
 });
-const config = { attributes: true, childList: true, characterData: true, subtree: true };
+const config = { attributes: false, childList: true, characterData: false, subtree: true };
 observer.observe(document.body, config);
